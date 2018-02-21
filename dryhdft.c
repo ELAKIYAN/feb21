fe5f0fbf4f0fa882ebece8e3291038e24a7e54b1
ELAKIYAN <31242650+ELAKIYAN@users.noreply.github.com>
2018-02-21 16:06:59 +0530

    Update dryhdft.c

--- dryhdft.c
+++ dryhdft.c
@@ -2,11 +2,11 @@
 
 int main(void) 
 {
-	int n;
-	scanf("%d",&n);
-	if(n%7==0)
-	printf("yes");
-	else
-	printf("no");
-	return 0;
+	 int n;
+	 scanf("%d",&n);
+	 if(n%7==0)
+	 printf("yes");
+	 else
+	 printf("no");
+	 return 0;
 }
